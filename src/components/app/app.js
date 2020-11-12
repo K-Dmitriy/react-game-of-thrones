42b14b86cf37e4c09dc2b42275659785a5e5f49d
Дмитрий Козлов <d.kozlov@hott.local>
2020-11-12 14:58:55 +0300

    add toggle random character

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,55 @@
-import React from 'react';
-import {Col, Row, Container} from 'reactstrap';
+import React, { Component } from 'react';
+import {Col, Row, Container, Button} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
+import './app.css';
 
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+export default class App extends Component {
+    constructor() {
+        super();
+        this.toggleActivity = this.toggleActivity.bind(this);
+    }
+
+    state = {
+        activityRandomChar: true
+    };
+
+    toggleActivity() {
+        this.setState({
+            activityRandomChar: !this.state.activityRandomChar
+        });
+    }
+
+    render() {
+        const {activityRandomChar} = this.state;
+
+        return (
+            <>
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                            <Button onClick={this.toggleActivity} color={activityRandomChar ? 'secondary' : 'primary'} block>
+                                Toggle random character
+                            </Button>
+                            {activityRandomChar ? <RandomChar/> : null}
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
 };
-
-export default App;
